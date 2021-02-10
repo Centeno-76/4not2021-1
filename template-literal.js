419a2492931af94f3bd58974f4d7e17d0a686372
Centeno-76 <mateuscenteno76@gmail.com>
2021-02-10 01:26:32 +0000

    (09/02) Início a revisão de JS

---
+++ template-literal.js
@@ -0,0 +1,6 @@
+let nome = 'joão'
+let idade = 23
+
+console.log(`meu nome é ${nome}, minha idade é ${idade}`)
+console.log(`Meu nome é ${nome.toUpperCase()} e minha idade é ${idade}.`)
+console.log(`7 vezes 6 é igual a ${7 * 6}.`)
