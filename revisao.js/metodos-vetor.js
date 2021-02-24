8a694d4c05d4e15932e8631cca0a9f2e5558b14b
Centeno-76 <mateuscenteno76@gmail.com>
2021-02-24 00:12:02 +0000

    (23/02) Término da revisão de JA

--- revisao.js/metodos-vetor.js
+++ revisao.js/metodos-vetor.js
@@ -42,3 +42,98 @@ const pets = [
     peso: 2.5
     }
 ]
+
+//find(); retorna a primeira ocorrência que corresponda ao critério informado
+//Ou seja acha o primeiro componente que corresponde a busca e o seleciona
+//Retorna undefined quando não encontra nenhum elemento coincidente
+console.log(pets.find(elemento => elemento.peso > 10))
+console.log(pets.find(pet => pet.idade < 4))
+console.log(pets.find(pet => pet.especie === 'gato' && pet.sexo == 'M'))
+console.log(pets.find(pet => pet.especie == 'calopsita'))
+
+console.log('------------------------------------')
+//some(): retorna true caso haja ALGUM elemento que corresponda ao critério
+// ou false caso contrário
+console.log(pets.some(pet => pet.nome === 'Cerberus'))
+console.log(pets.some(pet => pet.especie === 'tartaruga' && pet.sexo === 'F'))
+
+console.log('------------------------------------')
+
+//every(): retorna true caso TODOS os elementos correspondam ao critério
+//ou false caso contrário
+console.log(pets.every(pet => pet.especie === 'cão'))
+console.log(pets.every(pet => pet.peso >= 1))
+
+
+console.log('------------------------------------')
+
+//filter(): retorna um novo vetor contendo apenas os elementos que
+// correspondem ao critério passado
+console.log(pets.filter(x => x.especie === 'gato'))
+console.log(pets.filter(x => x.idade > 20))
+console.log(pets.filter(x => x.sexo === 'F' && x.especie ===  'papagaio'))
+
+console.log('------------------------------------')
+
+// map(): cria um novo vetor COM O MEMSO NÚMERO DE ELEMENTOS do vetor original,
+//correspondendo a algum tipo de transformação nos elementos originais
+
+//Novo vetor contendo apenas o nome dos pets, em maiúsculas
+console.log(pets.map(e => e.nome.toUpperCase()))
+
+const numeros = [2, 6, -3, 9, 5, -7, 1, 4]
+console.log(numeros)
+
+//Criando um novo vetor em que cada elemento corresponde ao elemento do 
+//vetor original elevando ao quadrado
+console.log(numeros.map(x => x ** 2))
+
+console.log('------------------------------------')
+
+//reduce(): reduz um vetor de valores a um único valor.
+//Parâmetros:
+// acumulador: resultado parcial até o momento
+//valor atual: o resultado correspondente ao elemento atual, que
+//Integrará o acumulador
+
+//Soma
+console.log(numeros.reduce((acum, val) => acum + val))
+
+//Multiplicação
+console.log(numeros.reduce((acum, val) => acum * val))
+
+console.log('------------------------------------')
+
+//Concatenação
+let p1 = 'Bom'
+let p3 = 'galera!'
+console.log([p1 ,' dia, ', p3].reduce((a, v) => a + v))
+
+console.log('------------------------------------')
+
+//SOMAR O PESOS DE TODOS OS PETS
+/*
+*/
+//Abordagem 1: criando um vetor auxiliar contendo apenas os pesos dos pets
+let pesos = pets.map(pet => pet.peso)
+console.log(pesos)
+console.log(pesos.reduce((acum, val) => acum + val))
+
+console.log('------------------------------------')
+
+//Abordagem 2: fazendo map() + reduce() em uma linha só
+console.log(pets.map(pet => pet.peso).reduce((acum, val) => acum + val))
+
+//Abordagem 3: reduce() com parâmetros extras
+//2º parâmetro do reduce(): valor inicial do acumulador
+console.log(pets.reduce((acum, val) => acum + val.peso, 0)) //a legibilidade fica um pouco ruim a abordagem 2 fica melhor
+
+console.log('------------------------------------')
+
+//includes(): retorna true caso o vetor inclua o valor informado
+console.log(numeros.includes(6))
+console.log(numeros.includes(11))
+
+//Diferença de some() e includes()
+//some() -> recebe uma função para testar a existência de uma condição
+//incluides() -> recebe um valor simples (primitivo) para testar a existência
